f6543ef9be259d5b2412ffca9db362ae193f1420
Eric Lui <eric.lui.2001@gmail.com>
2023-02-02 01:13:52 -0800

    Worked on clarifying assembly operations

--- program1.s
+++ program1.s
@@ -1,19 +1,52 @@
 # TODO: Write loop structure for 15 messages
-
+    swt                            # Switch register sets
+    li  0                          # Load immediate 0 into rM 
+    mov r4, rM                     # 
+    li  1                          # Load immediate 1 into rM
+    mov r5, rM                     #
+    nop                            # nop in order to match with Branch's power of 4
+    nop
+    nop
 Loop:
-
-# Load 0 into register, then we can call load
-# Do the same for 1
-
-ldb r0, mem[0]
-ldb r1, mem[1]   
-
-Zero out rX
-Move r1 to rY
-# p8
-Bit XOR with rX:0 and rY:0
-Bit XOR with rX:0 and rY:1
-Bit XOR with rX:0 and rY:2
+    mov r0, r4
+    mov r1, r5
+    swt                            # Switch register sets
+    lb  r0                         # Load mem[0] into rM
+    mov r0, rM                     # ldb r0, mem[0]
+    lb  r1                          # Load mem[1] into rM
+    mov r1, rM                     # ldb r1, mem[1]
+    li  0                          # Load 0 into rM
+    mov rY, rM                     # Clear out rY   
+    
+    # Calculating for p8
+    
+    # d9
+    mov rX, r0                     # Moves mem[0] into rX                               
+    lsl rX, 7                      # Keep only d9 from rX
+    lsr rX, 3                      # Places the wanted bit at bit 4 for p8
+    xor rY                         # XOR rX and rY, put into rY
+    lsr rX, 1                      # Places the wanted bit at bit 4 for p4
+    xor rY                         # XOR rX and rY, put into rY
+    lsr rX, 2                      # Places the wanted bit at bit 4 for p1
+    xor rY                         # XOR rX and rY, put into rY
+    lsr rX, 1                      # Places the wanted bit at bit 4 for p0
+    xor rY                         # XOR rX and rY, put into rY
+    
+    // TO DO:
+    # d10
+    mov rX, r0                     # Moves mem[0] into rX
+    lsr rX, 1                      # Shifts 1 to get rid of last bit          
+    lsl rX, 7                      # Keep only d10 from rX
+    lsr rX, 3                      # Places the wanted bit at bit 4 for p8
+    xor rY                         # XOR rX and rY, put into rY
+    lsr rX, 1                      # Places the wanted bit at bit 4 for p4
+    xor rY                         # XOR rX and rY, put into rY
+    lsr rX, 2                      # Places the wanted bit at bit 4 for p1
+    xor rY                         # XOR rX and rY, put into rY
+    lsr rX, 1                      # Places the wanted bit at bit 4 for p0
+    xor rY                         # XOR rX and rY, put into rY
+ 
+                                   # Bit XOR with rX:0 and rY:2
 
 # p4
 Bit XOR with rX:1 and rY:0
@@ -84,3 +117,18 @@ Bitwise XOR between rX and r1
 
 Store r0 as mem[30]
 Store r1 as mem[31]
+
+Iterate:
+    swt                 # Switch register sets
+    li  2
+    mov rY, rM          # 2
+    mov rX, r4          # 0
+    add rZ              # rX + rY
+    mov r4, rZ          
+    mov rX, r5
+    add rZ              # rX + rY after rX incremented
+    mov r5, rZ
+    li  30
+    mov rY, rM
+    sbflt               # rX < rY then branch
+    b $Loop
