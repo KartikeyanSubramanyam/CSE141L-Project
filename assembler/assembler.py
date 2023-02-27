ad05b2b2f64bc5e13e20f9237af6af66ead159ae
Akhilan Ganesh <aganesh@ucsd.edu>
2023-02-26 17:36:23 -0800

    Updated skeleton code for assembler

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -6,23 +6,34 @@ def read_file(file_name):
     # do file i/o here to read in the assembly code
     pass
 
+def write_file(file_name, file_contents):
+    # do file i/o here to write out the machine code
+    pass
+
 def file_lexer(fileLines):
     # the goal is to tokenize each line of the file
     lexed_tokens = []
     for line in fileLines:
         instruction_lexer(line);
 
+    return lexed_tokens
+
 def instruction_lexer(line):
+    # separate each instruction into tokens
     return ["placeholder", "r0", "r0"]
 
-def parser(tokenized_lines):
-    machine_code = []
+def file_parser(tokenized_lines):
+    machine_codes = [] # list of 9-bit machine codes
 
     for line in tokenized_lines:
         pass
+        # instruction_parser
+    
+    return machine_codes
 
 
 def instruction_parser(instruction_tokens):
+    # parse the instruction tokens to produce a 9-bit machine code
     machine_code = "000000000"
     operation = instruction_tokens[0]
     arguments = instruction_tokens[1:]
@@ -61,3 +72,5 @@ def instruction_parser(instruction_tokens):
         machine_code = "111111111"
     else:
         print("Error: no such instruction exists")
+    
+    return machine_code
